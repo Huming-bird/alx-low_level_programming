30dd56470e0553b5bec54f04c3ad9ef2e0b2ef9f
Huming-bird <sonugaahmed@gmail.com>
2022-12-08 07:57:54 -0800

    ahmed shell redirections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - start of program
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	printf("Size of a char: %c", (unsigned long)sizeof(char));
-	printf("Size of an int: %d", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu", (unsigned long)sizeof(long));
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(double));
-	printf("Size of a float: %f", (unsigned long)sizeof(float));
+	char d;
+	int n;
+	float p;
+	long z;
+	double v;
+	printf("Size of a char: %c", (unsigned long)sizeof(d)"\n");
+	printf("Size of an int: %d", (unsigned long)sizeof(n)"\n");
+	printf("Size of a long int: %lu", (unsigned long)sizeof(z)"\n");
+	printf("Size of a long long int: %lu", (unsigned long)sizeof(v)"\n");
+	printf("Size of a float: %f", (unsigned long)sizeof(p)"\n");
 	return (0);
 }
