671abdf111a2200b90f204708e0785e9412896ee
Huming-bird <sonugaahmed@gmail.com>
2022-12-08 07:17:33 -0800

    ahmed shell redirections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - start of program
+ * Return : always 0
+ */
+
 int main(void)
 
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return(0);
+	printf("with proper grammar, but the outcome is a piece of art");
+	return(0);
 }
