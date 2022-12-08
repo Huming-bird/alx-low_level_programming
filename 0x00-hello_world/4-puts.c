5bad804c9ad6197dbd400b307e7aaaa3aad77b54
Huming-bird <sonugaahmed@gmail.com>
2022-12-08 05:13:21 -0800

    ahmed shell redirections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
-    char alx = "Programming is like building a multilingual puzzl, \n"
+    char alx = "Programming is like building a multilingual puzzl, \n";
     
-    puts(alx)	   
-    return(0)
+    puts(alx);
+    return(0);
 }
