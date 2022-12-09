205539f7bf0bcc5d22208b0039936ad65657af6b
Huming-bird <sonugaahmed@gmail.com>
2022-12-09 20:47:11 +0000

    ahmed conditionals

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return: int
+ */
+
+int main(void)
+
+{
+	int a, b, c, d;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = '0'; b < '9'; b++)
+		{
+			for (c = '0'; c < '9'; c++)
+			{
+				for (d = c + 1; d <='9';d++)
+				{
+					if (b != d )
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+						putchar(',');
+						putchar('\n');
+				}
+			}
+		}
+	}
+}
