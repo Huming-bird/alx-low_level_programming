9338839eb6e450ef72a75bcd00a9277bf012c05e
Huming-bird <sonugaahmed@gmail.com>
2022-12-09 05:48:48 -0800

    ahmed conditionals

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start of prog
+ * return: always int
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - start of prog
+ * return: always int
+ */
+
+int main(void)
+
+{
+	int i;
+
+
+}
