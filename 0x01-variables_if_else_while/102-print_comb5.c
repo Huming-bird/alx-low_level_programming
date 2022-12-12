d230e3e1916eeb5d17260ed93ec195fb6b96831f
Huming-bird <117739572+Huming-bird@users.noreply.github.com>
2022-12-12 07:26:29 +0100

    ahmed conditionals

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - start
- * Return: int
- */
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
 
 int main(void)
-
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = '0'; b < '9'; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			for (c = '0'; c < '9'; c++)
-			{
-				for (d = c + 1; d <='9';d++)
-				{
-					if (b != d )
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-						putchar(',');
-					}
-				}
-			}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && b == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
