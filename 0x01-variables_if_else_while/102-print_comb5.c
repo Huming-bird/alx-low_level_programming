82649f970a6686b2a33abb670c2dbab3fcf11f00
Huming-bird <sonugaahmed@gmail.com>
2022-12-11 22:47:10 -0800

    ahmed conditionals

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,9 @@ int main(void)
 	int a, b;
 
 	for (a = '0'; a <= '9'; a++)
-	for (a = 0; a <= 98; a++)
+	{
+		for (a = 0; a <= 98; a++)
+		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
@@ -19,11 +21,13 @@ int main(void)
 			putchar((b % 10) + '0');
 
 			if (a == 98 && b == 99)
+			{
 				continue;
-
+			}
 			putchar(',');
 			putchar(' ');
+		}
+	}
 	putchar('\n');
-
 	return (0);
 }
