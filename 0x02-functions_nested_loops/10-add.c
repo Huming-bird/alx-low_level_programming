6f3ed53aaf193f9da5014ba71b0ad93633ae8907
Huming-bird <sonugaahmed@gmail.com>
2022-12-13 13:55:41 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - start
+ * @a: param
+ * @b: param
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	_putchar(c);
+	return (0);
+}
