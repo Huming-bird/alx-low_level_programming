22022e4dfe8099fdc0de1d7b21616023cdc1ddb4
Huming-bird <sonugaahmed@gmail.com>
2022-12-13 11:58:27 -0800

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - start
+ * @n: num
+ * Return: tyl int
+ *
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	a = n % 10;
+	return (a);
+}
