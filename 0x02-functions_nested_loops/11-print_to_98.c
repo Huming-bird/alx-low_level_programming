0fcce44a3c64180c261b247feb3b1579424acb84
Huming-bird <sonugaahmed@gmail.com>
2022-12-13 14:17:03 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n < 98)
 	{
@@ -31,7 +31,6 @@ int print_to_98(int n)
 			p--;
 		}
 	}
-	_putchar('\n');
-	return (0);
+	_putchar('\n);
 }
 
