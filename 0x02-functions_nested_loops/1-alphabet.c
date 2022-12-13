354f81e22d372cbe039acde2316fe4946c395803
Huming-bird <sonugaahmed@gmail.com>
2022-12-12 23:00:46 -0800

    nested loop tests

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start of prog
+ * Return: always int
+ */
+
+int maun(void)
+
+{
+	void print_alphabet(void);
+	{
+		int i;
+
+		i = 'a';
+
+		while i < 'z' + 1;
+		{
+			putchar(i);
+			putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}	
