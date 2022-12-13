5f827e0779f5eec5e2872c1ec4ba5662f1a1750e
Huming-bird <sonugaahmed@gmail.com>
2022-12-13 10:48:49 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,19 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char i;
+	int a;
 
+	a = 0;
+
+	while (a < 10)
+	{
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
-		_putchar(i);
+	}
 	}
 	_putchar('\n');
 }
