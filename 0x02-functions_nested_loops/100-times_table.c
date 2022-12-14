99d2d95d6f7c2ef33fbc9bab1fc842532911a5cb
Huming-bird <sonugaahmed@gmail.com>
2022-12-14 01:17:14 -0800

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,13 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
+	if (n == 0 || n > 15)
+	{
+		return (0);
+	}
+	else
+	{
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
@@ -19,22 +26,33 @@ void print_times_table(int n)
 		{
 			_putchar(k + '0');
 		}
-		if (k <= n && j != 0)
+		if (k <= 10 && j != 0)
 		{
 			_putchar(',');
+			_putchar(' ');
 			_putchar(' ');
 			_putchar(' ');
 			_putchar(k + '0');
 		}
-		else if (k >= n)
+		else if (k >= 10 && k < 100)
 		{
 			_putchar(',');
 			_putchar(' ');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		else if (k >= 100)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 100) + '0');
 			_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
 		}
 	}
 		_putchar('\n');
+	}
 	}
 }
 
