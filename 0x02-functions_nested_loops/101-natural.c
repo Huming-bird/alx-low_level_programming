ce9a4403215d5ce2e1431aa5227deb3791f75f42
Huming-bird <sonugaahmed@gmail.com>
2022-12-14 01:41:20 -0800

    updated

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * natural - start
+ * Return: 0/
+ */
+
+void natural(void)
+{
+	int n, sum;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d\n", sum);
+
+
