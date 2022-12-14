1648082760515cc1c2c0641e95b3878385c5ff55
Huming-bird <sonugaahmed@gmail.com>
2022-12-14 02:20:42 -0800

    updated

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum, n;
+	
+	n = 0;
+	sum = 1;
+	while (n < 50)
+	{
+		if (n == 0)
+		{
+			sum = sum + 1;
+		}
+		else
+		{
+			sum += sum;
+		}
+		putchar(sum + '0');
+		putchar('\n');
+		n++;
+	}
+}	
