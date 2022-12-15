ec0b606e6aab5746af1f807679fcfb51916eadd5
Huming-bird <sonugaahmed@gmail.com>
2022-12-15 10:24:31 -0800

    more nested loops and functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,20 +10,25 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
 			if (j >= 10)
 			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			}
-			else
-			{
-				_putchar(j);
+				_putchar((j / 10) % 10 + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+}
+
+int main(void)
+{
+	more_numbers();
+	return (0);
 }
