429904462e1e7fc75c549a5366e9a91707dd0fb0
Huming-bird <sonugaahmed@gmail.com>
2022-12-15 14:05:44 -0800

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,16 +14,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
 			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
+			_putchar(j % 10 + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
