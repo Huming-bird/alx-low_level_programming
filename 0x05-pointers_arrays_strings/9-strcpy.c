517f8023cd69906aa1792cef38787a0e59a7c188
Huming-bird <sonugaahmed@gmail.com>
2022-12-19 04:48:58 -0800

    updated

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strcpy - start
+ * @dest: param 1
+ * @src: param 2
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a, b;
+
+	while (*(src + a) != '\0')
+	{
+		a++
+	}
+	for ( ; a < b ; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
