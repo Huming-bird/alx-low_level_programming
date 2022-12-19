2c45376634bdb5647d3699dffd653d40c40bbc85
Huming-bird <sonugaahmed@gmail.com>
2022-12-19 03:48:32 -0800

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - start
+ * @str: param1
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(str++);
+	}
+	_putchar('\n');
+}
