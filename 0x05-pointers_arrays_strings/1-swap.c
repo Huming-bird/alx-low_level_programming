04fe8ab6ed2350444d5d9a3e6e381d6a8c96d435
Huming-bird <sonugaahmed@gmail.com>
2022-12-19 02:38:38 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - start of prog
+ * @a: param 1
+ * @b: param 2
+ * Return: void
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
