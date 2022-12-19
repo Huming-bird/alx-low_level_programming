12f14ed2338930c9f586fcd0affef0eecdb62062
Huming-bird <sonugaahmed@gmail.com>
2022-12-19 02:26:42 -0800

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - start of prog
- * @*n: pointer 1
+ * reset_to_98 - start of prog
+ * @n: pointer 1
  * Return: int type
  */
 void reset_to_98(int *n)
 {
 	int p;
+
 	n = &n;
 	*n = 98;
 }
