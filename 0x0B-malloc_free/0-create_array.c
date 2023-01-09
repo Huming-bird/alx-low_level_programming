9ae287dba7901fc53200eeb91ac571f1609f6e00
Huming-bird <sonugaahmed@gmail.com>
2023-01-09 07:42:46 -0800

    memory allocations

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,15 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int a;
 
-	s = malloc(sizeof(char) * size);
+	s = (char *) malloc(sizeof(char) * size);
 	if (size == 0 || s == NULL)
 	{
 		return (NULL);
 	}
 	for (a = 0; a < size; a++)
 	{
-		s[a] = c;
+		*(s + a) = c;
 	}
+	*(s + a) = '\0';
 	return (s);
 }
