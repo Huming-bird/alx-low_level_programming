7a6361b6a911a065f364f9f02493265a076788e6
Huming-bird <sonugaahmed@gmail.com>
2023-01-09 07:49:14 -0800

    memory allocations

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
