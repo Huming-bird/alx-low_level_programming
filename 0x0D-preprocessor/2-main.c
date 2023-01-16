81663a85c08697eb31b995eac49bfbc0b5e85991
Huming-bird <sonugaahmed@gmail.com>
2023-01-16 04:03:43 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * makin - prints the name of a file
+ * Return int
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef sum_h
+#define sum_h
+
+#define sum(x,y) ((x) + (y))
+#endif
