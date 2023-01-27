ed94f0b8274c56d67188ec6e0098f0e0e8492c2a
Huming-bird <sonugaahmed@gmail.com>
2023-01-26 23:39:29 -0800

    updates to lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
 
 /**
  * print_list - prints all the elements of a linked list
@@ -23,6 +9,7 @@ typedef struct list_s
  *
  * Return: the number of nodes printed
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t a = 0;
